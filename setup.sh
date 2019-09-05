ee8f7353a423882902eb10c8ec44c078eeaad005
wesr <wesr@zhaw.ch>
2019-09-05 13:04:16 +0200

    added script for setting environment variables

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ARCH=arm64
+
+#export CROSS_COMPILE=/home/wesr/nvidia_gcc/install/bin/aarch64-unknown-linux-gnu-
+export CROSS_COMPILE=/home/wesr/nvidia_gcc/gcc-linaro-6.4.1/bin/aarch64-linux-gnu-
+
